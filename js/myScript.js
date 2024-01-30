e63e19a8d2c8c1c40001f21b0d2dabb0c0db6d6a
zaman20 <adorzaman18@gmail.com>
2024-01-31 00:37:30 +0600

    Multistep form design done

--- js/myScript.js
+++ js/myScript.js
@@ -1,4 +1,18 @@
 $(document).ready(function(){
-   
+    let pageNum = 1;
+   $('.next').on('click',function(){
+        let id = $(this).data('id');
+        let next = id+1;
+        let prev = id;
+        
+        let nextPage = '#page'+next;
+        let prevPage = '#page'+prev;
+
+        $(nextPage).show();
+        $(nextPage).css('left','0');
+        $(nextPage).css('visibility','visible');
+        $(prevPage).hide();
+
+   })
 
 });
