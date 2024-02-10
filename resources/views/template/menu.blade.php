3370011281a15e49a22aaf7c006822c960091e4f
zaman20 <adorzaman18@gmail.com>
2024-02-10 21:18:44 +0600

    Menu active system done

--- resources/views/template/menu.blade.php
+++ resources/views/template/menu.blade.php
@@ -1,17 +1,31 @@
 
 <ul>
     @if(session('type')=='author')
-    <li><a href="{{url('/author-submit')}}">Submit New Manuscript</a></li>
-    <li><a href="{{url('/author-incomplete-submission')}}">Submissions Need Revision</a></li>
-    <li><a href="">Incomplete Submissions</a></li>
-    <li><a href="{{url('/author-pending-paper')}}">Submissions Waiting for Editor Approval</a></li>
-    <li><a href="">Submissions Being Processed</a></li>
-    <li><a href="{{url('/author-dashboard')}}">Author Main Page</a></li>
+    <li class="{{ Request::routeIs('dashboard') ? 'menuactive' : '' }}">
+        <a href="{{route('dashboard')}}">Author Main Page</a></li>
+    <li class="{{ Request::routeIs('author1') ? 'menuactive' : '' }}">
+        <a href="{{url('/author-submit')}}">Submit New Manuscript</a></li>
+    <li class="{{ Request::routeIs('author2') ? 'menuactive' : '' }}">
+        <a href="">Submissions Need Revision</a></li>
+    <li class="{{ Request::routeIs('author3') ? 'menuactive' : '' }}">
+        <a href="">Incomplete Submissions</a></li>
+    <li class="{{ Request::routeIs('author4') ? 'menuactive' : '' }}">
+        <a href="{{url('/author-incomplete-submission')}}"><a href="{{url('/author-pending-paper')}}">Submissions Waiting for Editor Approval</a></li>
+    <li class="{{ Request::routeIs('author5') ? 'menuactive' : '' }}">
+        <a href="">Submissions Being Processed</a></li>
+   
     @elseif(session('type')=='editor')
-    <li><a href="{{url('/editor-pending-paper')}}">Papers Need Review</a></li>
-    <li><a href="{{url('/reviewers')}}">Reveiwers</a></li>
-    <li><a href="{{url('/incomplete-submission')}}">Submissions in Revission</a></li>
-    <li><a href="">Submissions Waiting for Approval</a></li>
-    <li><a href="">Submissions after Edit</a></li>
+    <li class="{{ Request::routeIs('editor1') ? 'menuactive' : '' }}">
+        <a href="{{url('/editor-dashboard')}}">Dashboard</a></li>
+    <li class="{{ Request::routeIs('editor2') ? 'menuactive' : '' }}">
+        <a href="{{url('/editor-pending-paper')}}">Papers Need Review</a></li>
+    <li class="{{ Request::routeIs('editor3') ? 'menuactive' : '' }}">>
+        <a href="{{url('/reviewers')}}">Reveiwers</a></li>
+    <li class="{{ Request::routeIs('editor4') ? 'menuactive' : '' }}">>
+        <a href="{{url('/incomplete-submission')}}">Submissions in Revission</a></li>
+    <li class="{{ Request::routeIs('editor') ? 'menuactive' : '' }}">>
+        <a href="">Submissions Waiting for Approval</a></li>
+    <li class="{{ Request::routeIs('editor') ? 'menuactive' : '' }}">>
+        <a href="">Submissions after Edit</a></li>
     @endif
 </ul>
--- routes/web.php
+++ routes/web.php
@@ -16,24 +16,25 @@ use App\Http\Controllers\AppController;
 Route::get('/', function () {
     return view('login');
 });
-Route::get('/author-dashboard',[AppController::class,'authorDashboard']);
-Route::get('/author-submit',[AppController::class,'authorSubmit']);
+Route::get('/author-dashboard',[AppController::class,'authorDashboard'])->name('dashboard');
+Route::get('/author-submit',[AppController::class,'authorSubmit'])->name('author1');
 Route::post('/paper-submit',[AppController::class,'paperSubmit']);
-Route::get('/author-pending-paper',[AppController::class,'authorPending']);
+Route::get('/author-pending-paper',[AppController::class,'authorPending'])->name('author4');
 Route::get('/paper-view/{id}',[AppController::class,'paperView']);
 Route::get('/author-register',[AppController::class,'authorRegisterPage']);
 Route::post('/add-author',[AppController::class,'addAuthor']);
 Route::post('/auth-login',[AppController::class,'loginAuth']);
 Route::get('/logout',[AppController::class,'logout']);
-Route::get('/editor-dashboard',[AppController::class,'editorDashboard']);
-Route::get('/editor-pending-paper',[AppController::class,'editorPendingPaper']);
+
+Route::get('/editor-dashboard',[AppController::class,'editorDashboard'])->name('editor1');
+Route::get('/editor-pending-paper',[AppController::class,'editorPendingPaper'])->name('editor2');
 Route::post('/editor-comment',[AppController::class,'editorComment']);
 Route::post('/editor-to-revision',[AppController::class,'editortoRevision']);
-Route::get('/incomplete-submission',[AppController::class,'incompleteSubmission']);
+Route::get('/incomplete-submission',[AppController::class,'incompleteSubmission'])->name('editor4');
 Route::get('/incomplete-submission',[AppController::class,'incompleteSubmission']);
 Route::get('/author-incomplete-submission',[AppController::class,'authorIncompleteSubmission']);
 Route::post('/add-reviewer',[AppController::class,'addReviewer']);
-Route::get('/reviewers',[AppController::class,'reviewers']);
+Route::get('/reviewers',[AppController::class,'reviewers'])->name('editor3');
 Route::post('/dlt-reviewer',[AppController::class,'dltReviewer']);
 Route::post('/dlt-paper',[AppController::class,'dltPaper']);
 
